f691548f17f656874408cd3905c47ea1ab8fc30f
vehidtr <vehid.trtak@gmail.com>
2024-07-23 12:34:37 +0200

    Update domain name middleware

--- middleware.js
+++ middleware.js
@@ -2,8 +2,8 @@ export async function middleware(req) {
   const url = new URL(req.url);
 
   if (url.pathname.startsWith("/app")) {
-    return fetch(`https://app.example.com${url.pathname}`);
+    return fetch(`https://app.moonastudios.com${url.pathname}`);
   }
 
-  return fetch(`https://landing.example.com${url.pathname}`);
+  return fetch(`https://landing.moonastudios.com${url.pathname}`);
 }
